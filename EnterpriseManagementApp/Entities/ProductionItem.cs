89841c8198f3a7e4f4c8b02a79039b2f4d736943
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-09 01:55:10 +0100

    The first entities were created.

---
+++ EnterpriseManagementApp/Entities/ProductionItem.cs
@@ -0,0 +1,19 @@
+﻿namespace EnterpriseManagementApp.Entities
+{
+    public class ProductionItem
+    {
+        public Guid Id { get; set; }
+        public String Type { get; set; }
+        public String Material { get; set; }
+        public int Thickness { get; set; }
+        public int Length { get; set; }
+        public int Diameter { get; set; }
+        public int QuantityPCS { get; set; }
+        public int QuantityPallets { get; set; }
+        public int HallNumber { get; set; }
+        public int Foreman { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public String? AdditionalInformation { get; set; }
+        public bool ReadyToPickUp { get; set; }
+    }
+}
---
+++ EnterpriseManagementApp/Entities/Type.cs
@@ -0,0 +1,9 @@
+﻿namespace EnterpriseManagementApp.Entities
+{
+    public class Type
+    {
+        public Guid Id { get; set; }
+        public String Value { get; set; }
+        public Guid IdOfItem { get; set; }
+    }
+}
