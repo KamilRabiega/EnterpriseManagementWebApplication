d8af4d7c53bf24f1b9a73fc855108f971fb5394a
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-09 14:41:05 +0100

    Fully working Add page (production role).

--- EnterpriseManagementApp/Pages/Production/Products/Add.cshtml.cs
+++ EnterpriseManagementApp/Pages/Production/Products/Add.cshtml.cs
@@ -1,3 +1,6 @@
+using EnterpriseManagementApp.Data;
+using EnterpriseManagementApp.Entities;
+using EnterpriseManagementApp.Entities.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,39 @@ namespace EnterpriseManagementApp.Pages.Production.Products
 {
     public class AddModel : PageModel
     {
+        private readonly EmaDbContext emaDbContext;
+
+        [BindProperty]
+        public AddProductionItem AddProductionItemRequest { get; set; }
+
+        public AddModel(EmaDbContext emaDbContext)
+        {
+            this.emaDbContext = emaDbContext;
+        }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            var productionItem = new ProductionItem()
+            {
+                Type = AddProductionItemRequest.Type,
+                Material = AddProductionItemRequest.Material,
+                Thickness = AddProductionItemRequest.Thickness,
+                Length = AddProductionItemRequest.Length,
+                Diameter = AddProductionItemRequest.Diameter,
+                QuantityPCS = AddProductionItemRequest.QuantityPCS,
+                QuantityPallets = AddProductionItemRequest.QuantityPallets,
+                HallNumber = AddProductionItemRequest.HallNumber,
+                Foreman = AddProductionItemRequest.Foreman,
+                ProductionDate = AddProductionItemRequest.ProductionDate,
+                AdditionalInformation = AddProductionItemRequest.AdditionalInformation,
+                ReadyToPickUp = AddProductionItemRequest.ReadyToPickUp,
+            };
+            emaDbContext.ProductionItems.Add(productionItem);
+            emaDbContext.SaveChanges();
         }
     }
 }
