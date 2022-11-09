7fe04102fdc5be3f1bae49ad7924272bd5fd2302
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-09 01:56:32 +0100

    A folder for storage DbContext files have been created with first EmaDbContext.cs.

---
+++ EnterpriseManagementApp/Data/EmaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EnterpriseManagementApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseManagementApp.Data
+{
+    public class EmaDbContext : DbContext
+    {
+        public EmaDbContext(DbContextOptions<EmaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductionItem> ProductionItems { get; set; }
+        public DbSet<Entities.Type> Type { get; set; }
+        public DbSet<Hall> Halls { get; set; }
+        public DbSet<Material> Materials { get; set; }
+    }
+}
