ac45399d9b05d3209f658a5c122b4443f417ee00
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-09 14:39:21 +0100

    ViewModel was created for adding products (by production role).

---
+++ EnterpriseManagementApp/Entities/ViewModels/AddProductionItem.cs
@@ -0,0 +1,18 @@
+﻿namespace EnterpriseManagementApp.Entities.ViewModels
+{
+    public class AddProductionItem
+    {
+        public String Type { get; set; }
+        public String Material { get; set; }
+        public int Thickness { get; set; }
+        public int Length { get; set; }
+        public int Diameter { get; set; }
+        public int QuantityPCS { get; set; }
+        public int QuantityPallets { get; set; }
+        public int HallNumber { get; set; }
+        public int Foreman { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public String? AdditionalInformation { get; set; }
+        public bool ReadyToPickUp { get; set; }
+    }
+}
