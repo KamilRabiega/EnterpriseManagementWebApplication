e92f040373b243618a7353349bf5ad6b482e8038
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-09 22:23:27 +0100

    Search page was added (just a pattern, not working yet).

---
+++ EnterpriseManagementApp/Pages/Production/Products/Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnterpriseManagementApp.Pages.Production.Products
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
