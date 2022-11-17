89e1773c22f855e8a448715cbab5fa8e4f7d4013
Kamil Rabiega <63099679+KamilRabiega@users.noreply.github.com>
2022-11-18 00:48:49 +0100

    Register page for admin (to create a new users).

---
+++ EnterpriseManagementApp/Pages/Register.cshtml.cs
@@ -0,0 +1,58 @@
+using EnterpriseManagementApp.Entities.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnterpriseManagementApp.Pages
+{
+    [Authorize]
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userMaganer;
+
+        [BindProperty]
+        public Register RegisterViewModel { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userMaganer)
+        {
+            this.userMaganer = userMaganer;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var user = new IdentityUser
+            {
+                UserName = RegisterViewModel.Username,
+                Email = RegisterViewModel.Email
+            };
+
+            var identityResult = await userMaganer.CreateAsync(user, RegisterViewModel.Password);
+
+            if (identityResult.Succeeded)
+            {
+                var selectedRole = await userMaganer.AddToRoleAsync(user, RegisterViewModel.Role);
+
+                if (selectedRole.Succeeded)
+                {
+                    ViewData["Notification"] = new Notification
+                    {
+                        Type = Enums.NotificationType.Success,
+                        Message = "User was created successfully."
+                    };
+                    return Page();
+                }
+            }
+            ViewData["Notification"] = new Notification
+            {
+                Type = Enums.NotificationType.Error,
+                Message = "User wasn't created successfully, try again or contact to our support."
+            };
+            return Page();
+        }
+    }
+}
